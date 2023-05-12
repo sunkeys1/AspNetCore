f142c6d76836ea875faffb65a5f8607e4ac530f2
sunkeys1 <basatax5@gmail.com>
2023-05-12 17:57:13 +0700

    startup
    just prepare to start up

---
+++ WebApiNetCore/WebApiNetCore/Data/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiNetCore.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int GroupId { get; set; }
+        public int StateId { get; set; }
+    }
+}
